f664194df441ca724e1060e3cf76998646f5a426
Michael Täge <mictaege@googlemail.com>
2017-11-05 02:07:15 +0100

    Update to jitter v1.2

--- src/main/java/com/github/mictaege/eval/jitter/Dao.java
+++ src/main/java/com/github/mictaege/eval/jitter/Dao.java
@@ -6,15 +6,13 @@ import com.github.mictaege.jitter.api.OnlyIf;
 import java.util.List;
 
 import static com.github.mictaege.eval.jitter.BearerType.ARIANE5;
-import static com.github.mictaege.eval.jitter.BearerType.ATLAS;
-import static com.github.mictaege.eval.jitter.BearerType.TITAN;
 import static com.github.mictaege.eval.jitter.Flavour.ESA;
 import static com.github.mictaege.eval.jitter.Flavour.NASA;
 import static com.github.mictaege.eval.jitter.Flavour.ROSKOSMOS;
 import static java.util.Arrays.asList;
 
 @Alter(ifActive = ESA, with = "EsaDao", nested = true)
-@Alter(ifActive = NASA, with = "NasaDao", nested = true)
+@Alter(ifActive = NASA, with = "com.github.mictaege.eval.jitter.nasa.NasaDao")
 @Alter(ifActive = ROSKOSMOS, with = "RoskosmosDao")
 public class Dao implements DaoIF {
 
@@ -27,13 +25,6 @@ public class Dao implements DaoIF {
     static class EsaDao implements DaoIF {
         public List<BearerType> findBearers() {
             return asList(ARIANE5);
-        }
-    }
-
-    @OnlyIf(NASA)
-    static class NasaDao implements DaoIF {
-        public List<BearerType> findBearers() {
-            return asList(ATLAS, TITAN);
         }
     }
 
---
+++ src/test/java/com/github/mictaege/eval/jitter/nasa/package-info.java
@@ -0,0 +1,6 @@
+@OnlyIf(NASA)
+package com.github.mictaege.eval.jitter.nasa;
+
+import com.github.mictaege.jitter.api.OnlyIf;
+
+import static com.github.mictaege.eval.jitter.Flavour.NASA;
