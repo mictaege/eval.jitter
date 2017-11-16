9e9a41ecf0b2fed8d5fdcd686cf6d9f782c9478c
Michael Täge <mictaege@googlemail.com>
2017-11-16 16:54:45 +0100

    Update jitter-plugin v1.3

---
+++ src/main/java/com/github/mictaege/eval/jitter/TowerControlMain.java
@@ -0,0 +1,10 @@
+package com.github.mictaege.eval.jitter;
+
+import javafx.application.Application;
+
+public class TowerControlMain {
+
+    public static void main(String[] args) {
+        Application.launch(TowerControlApp.class, args);
+    }
+}
