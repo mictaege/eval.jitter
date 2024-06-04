94e58ea944dd9c017bee63c1a7bef92070a94b55
Michael Taege <mictaege@googlemail.com>
2024-06-04 19:59:44 +0200

    updated the javaparser to version 3.26.0 in order to support Java Language Level 21.

--- src/model/java/com/github/mictaege/eval/jitter/SpaceShip.java
+++ src/model/java/com/github/mictaege/eval/jitter/SpaceShip.java
@@ -1,4 +1,8 @@
 package com.github.mictaege.eval.jitter;
 
-public record SpaceShip(SpaceShipType type) {
+public record SpaceShip(SpaceShipType type) implements Component {
+    @Override
+    public String getName() {
+        return type.getName();
+    }
 }
